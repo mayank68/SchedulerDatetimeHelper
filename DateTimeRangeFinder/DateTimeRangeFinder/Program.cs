e454a7c7d9112d1650edd4d2227ecb7dd7b5d8eb
mayank <mayank.developer@webzlabz.com>
2018-07-23 19:05:59 +0530

    InitCommit

---
+++ DateTimeRangeFinder/DateTimeRangeFinder/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeRangeFinder
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DateInput dateInput = new DateInput()
+            {
+                StartDateTime = DateTime.Now,
+                EndDateTime = DateTime.Now.AddHours(3).AddMinutes(30),
+                UntillDate = DateTime.Now.AddDays(230),
+                DaysOfWeek = "MO,TU,WE",
+                isEndDateDefined = true,
+                frequency=Helper.Frequency.Monthly,
+                Occurance=5,
+                DayName= "Sunday",
+                DayFormat= "fourth",
+                WeekDay=2,
+                isWeekDay=false
+
+
+            };
+            Console.WriteLine("-------------Input-----------------");
+            Console.WriteLine("start time:"+ dateInput.StartDateTime);
+            Console.WriteLine("end time:" + dateInput.StartDateTime);
+            Console.WriteLine("untill date:" + dateInput.UntillDate);
+            Console.WriteLine("days of week:" + dateInput.DaysOfWeek);
+            Console.WriteLine("End date:" + dateInput.isEndDateDefined);
+            Console.WriteLine("Occurance:" + dateInput.Occurance);
+            Console.WriteLine("day name:" + dateInput.DayName);
+            Console.WriteLine("day format:" + dateInput.DayFormat);
+            Console.WriteLine("-------------Input-----------------");
+            //var output = Helper.DateTimeHelper.FindDateRangeDailyAndWeekly(dateInput.isEndDateDefined, dateInput.StartDateTime, dateInput.EndDateTime, dateInput.Occurance, dateInput.UntillDate, dateInput.frequency, dateInput.DaysOfWeek, 3);
+            var output = Helper.DateTimeHelper.FindDateRange_Monthly(dateInput.isEndDateDefined, dateInput.StartDateTime, dateInput.EndDateTime, dateInput.Occurance, dateInput.UntillDate, dateInput.frequency, dateInput.DaysOfWeek,24,false,dateInput.DayFormat, dateInput.DayName, dateInput.isWeekDay, dateInput.WeekDay);
+            Console.WriteLine("-------------OutPut-----------------");
+            foreach (var item in output)
+            {
+                Console.WriteLine("----"+item.StartDate+"-------"+item.EndDate+"----");
+            }
+            Console.WriteLine("--------------end-------------------");
+            Console.Read();
+        }
+    }
+    public class DateInput
+    {
+        public DateTime StartDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
+        public DateTime UntillDate { get; set; }
+        public string DaysOfWeek { get; set; }
+        public Helper.Frequency frequency { get; set; }
+        public bool isEndDateDefined { get; set; }
+        public int Occurance { get; set; }
+        public string DayName { get; set; }
+        public string DayFormat { get; set; }
+        public int WeekDay { get; set; }
+        public bool isWeekDay { get; set; }
+    }
+}
---
+++ DateTimeRangeFinder/Helper/DateTimeHelper.cs
@@ -0,0 +1,350 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helper
+{
+    public enum Frequency
+    {
+        [Description("Everyday")]
+        EveryDay = 0,
+        [Description("Weekly")]
+        Weekly = 1,
+        [Description("Monthly")]
+        Monthly = 4,
+        [Description("Yearly")]
+        Yearly = 5
+    }
+
+    public class DateRange
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+
+    public static class DateTimeHelper
+    {
+        //Scheduler Add format as follow :
+        //FREQ = WEEKLY; COUNT = 5; BYDAY = MO,TU,WE,TH,FR
+
+        #region Daily and weekly
+        //covered daily weeekly
+        public static List<DateRange> FindDateRangeDailyAndWeekly(bool isEndDate, DateTime startDate, DateTime endDate, int noOfOccurance, DateTime utnillDate, Frequency frequency, string days, int dayofMonth)
+        {
+            try
+            {
+                List<DateRange> lstDateRange = new List<DateRange>();
+                List<DateRange> lstFinalDateRange = new List<DateRange>();
+                TimeSpan timeSpan = endDate - startDate;
+                List<DateTime> StartdateTimeRange = new List<DateTime>();
+                List<DateTime> EnddateTimeRange = new List<DateTime>();
+                if (!isEndDate)
+                {
+                    if (frequency == Frequency.EveryDay)
+                    {
+                        utnillDate = startDate.AddDays(noOfOccurance - 1);
+                    }
+                    if (frequency == Frequency.Weekly)
+                    {
+                        utnillDate = startDate.AddDays((noOfOccurance * 7) - 1);
+                    }
+                    if (frequency == Frequency.Monthly)
+                    {
+                        utnillDate = startDate.AddMonths(noOfOccurance);
+                    }
+                }
+                for (DateTime date = startDate; date.Date <= utnillDate.Date; date = date.AddDays(1))
+                {
+                    DateRange dateRange = new DateRange();
+                    dateRange.StartDate = date;
+                    dateRange.EndDate = date.AddDays(timeSpan.Days).AddHours(timeSpan.Hours).AddMinutes(timeSpan.Minutes);
+                    lstDateRange.Add(dateRange);
+                }
+                lstFinalDateRange = FilterFrequnecy_WeeklyAndDaily(lstDateRange, frequency, days, dayofMonth);
+                return lstFinalDateRange;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        #endregion
+
+        public static List<DateRange> FilterFrequnecy_WeeklyAndDaily(List<DateRange> list, Frequency frequency, string days, int dayOfMonth)
+        {
+            List<DateRange> lstDateTimeRange = new List<DateRange>();
+            foreach (var item in list)
+            {
+                List<string> strNewList = new List<string>();
+                string[] split = days.Split(',');
+                if (frequency == Frequency.Weekly)
+                {
+                    foreach (var itemDays in split)
+                    {
+                        string Newitem;
+                        if (itemDays.Equals("SU"))
+                        {
+                            Newitem = "Sunday";
+                            strNewList.Add(Newitem);
+                        }
+                        if (itemDays.Equals("MO"))
+                        {
+                            Newitem = "Monday";
+                            strNewList.Add(Newitem);
+                        }
+                        if (itemDays.Equals("TU"))
+                        {
+                            Newitem = "Tuesday";
+                            strNewList.Add(Newitem);
+                        }
+                        if (itemDays.Equals("WE"))
+                        {
+                            Newitem = "Wednesday";
+                            strNewList.Add(Newitem);
+                        }
+                        if (itemDays.Equals("TH"))
+                        {
+                            Newitem = "Thursday";
+                            strNewList.Add(Newitem);
+                        }
+                        if (itemDays.Equals("FR"))
+                        {
+                            Newitem = "Friday";
+                            strNewList.Add(Newitem);
+                        }
+                        if (itemDays.Equals("SA"))
+                        {
+                            Newitem = "Saturday";
+                            strNewList.Add(Newitem);
+                        }
+                    }
+                    foreach (var itemLst in strNewList)
+                    {
+                        if (item.StartDate.DayOfWeek.ToString() == itemLst.ToString())
+                        {
+                            lstDateTimeRange.Add(item);
+                        }
+                    }
+                }
+                if (frequency == Frequency.EveryDay)
+                {
+                    lstDateTimeRange = list;
+                }
+            }
+            return lstDateTimeRange;
+        }
+
+        #region monthly
+
+        public static bool NthDayOfMonthNumber(DateTime date, int dayNumber, string dayName)
+        {
+            int n = 0;
+            if (dayName.ToLower().ToLower() == "first")
+                n = 1;
+            else if (dayName.ToLower().ToLower() == "second")
+                n = 2;
+            else if (dayName.ToLower().ToLower() == "third")
+                n = 3;
+            else if (dayName.ToLower().ToLower() == "fourth")
+                n = 4;
+            int d = date.Day;
+            return (int)date.DayOfWeek == dayNumber && (d - 1) / 7 == (n - 1);
+        }
+        public static bool NthDayOfMonth(DateTime date, DayOfWeek dow, string dayName)
+        {
+            int n = 0;
+            if (dayName.ToLower().ToLower() == "first")
+                n = 1;
+            else if (dayName.ToLower().ToLower() == "second")
+                n = 2;
+            else if (dayName.ToLower().ToLower() == "third")
+                n = 3;
+            else if (dayName.ToLower().ToLower() == "fourth")
+                n = 4;
+            int d = date.Day;
+            return date.DayOfWeek == dow && (d - 1) / 7 == (n - 1);
+        }
+        public static List<DateRange> FindDateRange_Monthly(bool isEndDate, DateTime startDate, DateTime? endDate, int noOfOccurance, DateTime utnillDate, Frequency frequency, string days, int dayofMonth, bool isDayOfMonth, string weekNumber, string dayName,bool isWeekDay,int weekDay)
+        {
+            try
+            {
+                List<DateRange> lstDateRange = new List<DateRange>();
+                List<DateRange> lstFinalDateRange = new List<DateRange>();
+                TimeSpan timeSpan = endDate.Value - startDate;
+                List<DateTime> StartdateTimeRange = new List<DateTime>();
+                List<DateTime> EnddateTimeRange = new List<DateTime>();
+                if (!isEndDate)
+                {
+                    if (frequency == Frequency.Monthly)
+                    {
+                        utnillDate = startDate.AddMonths(noOfOccurance);
+                    }
+                }
+                for (DateTime date = startDate; date.Date <= utnillDate.Date; date = date.AddDays(1))
+                {
+                    DateRange dateRange = new DateRange();
+                    dateRange.StartDate = date;
+                    dateRange.EndDate = date.AddDays(timeSpan.Days).AddHours(timeSpan.Hours).AddMinutes(timeSpan.Minutes);
+                    lstDateRange.Add(dateRange);
+                }
+                lstFinalDateRange = FilterFrequnecy_Monthly(lstDateRange, frequency, days, dayofMonth, isDayOfMonth, weekNumber, dayName,isWeekDay,weekDay);
+                return lstFinalDateRange;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public static List<DateRange> FilterFrequnecy_Monthly(List<DateRange> list, Frequency frequency, string days, int dayOfMonth, bool isDayOfMonth, string weekNumber, string dayName,bool isWeekDay,int weekDay)
+        {
+            List<DateRange> lstDateRange = new List<DateRange>();
+            List<DateRange> FirstlstDateRange = new List<DateRange>();
+
+
+            if (frequency == Frequency.Monthly)
+            {
+                foreach (var item in list)
+                {
+                    if (isDayOfMonth)
+                    {
+                        if (dayOfMonth <= 31)
+                        {
+                            if (item.StartDate.Day == dayOfMonth)
+                            {
+                                lstDateRange.Add(item);
+                            }
+                        }
+                        else
+                        {
+                            //Todo exception message.
+                        }
+
+                    }
+                    else
+                    {
+                        if (item.StartDate.DayOfWeek.ToString() == dayName)
+                        {
+                            CheckWeekDay(weekNumber, dayName, lstDateRange, item,isWeekDay, weekDay);
+
+                        }
+
+                    }
+                }
+            }
+            return lstDateRange;
+        }
+        private static void CheckWeekDay(string weekNumber, string dayName, List<DateRange> lstDateRange, DateRange item,bool isWeekDay,int weekDay)
+        {
+            if(isWeekDay)
+            {
+                if (dayName.ToString().ToLower() == "monday")
+                {
+                    if (NthDayOfMonthNumber(item.StartDate, weekDay, weekNumber))
+                    {
+                        lstDateRange.Add(item);
+                    }
+                }
+                if (dayName.ToString().ToLower() == "tuesday")
+                {
+                    if (NthDayOfMonthNumber(item.StartDate, weekDay, weekNumber))
+                    {
+                        lstDateRange.Add(item);
+                    }
+                }
+                if (dayName.ToString().ToLower() == "wednesday")
+                {
+                    if (NthDayOfMonthNumber(item.StartDate, weekDay, weekNumber))
+                    {
+                        lstDateRange.Add(item);
+                    }
+                }
+                if (dayName.ToString().ToLower() == "thursday")
+                {
+                    if (NthDayOfMonthNumber(item.StartDate, weekDay, weekNumber))
+                    {
+                        lstDateRange.Add(item);
+                    }
+                }
+                if (dayName.ToString().ToLower() == "friday")
+                {
+                    if (NthDayOfMonthNumber(item.StartDate, weekDay, weekNumber))
+                    {
+                        lstDateRange.Add(item);
+                    }
+                }
+                if (dayName.ToString().ToLower() == "saturday")
+                {
+                    if (NthDayOfMonthNumber(item.StartDate, weekDay, weekNumber))
+                    {
+                        lstDateRange.Add(item);
+                    }
+                }
+                if (dayName.ToString().ToLower() == "sunday")
+                {
+                    if (NthDayOfMonthNumber(item.StartDate, weekDay, weekNumber))
+                    {
+                        lstDateRange.Add(item);
+                    }
+                }
+            }
+            else
+            {
+                if (dayName.ToString().ToLower() == "monday")
+                {
+                    if (NthDayOfMonth(item.StartDate, DayOfWeek.Monday, weekNumber))
+                    {
+                        lstDateRange.Add(item);
+                    }
+                }
+                if (dayName.ToString().ToLower() == "tuesday")
+                {
+                    if (NthDayOfMonth(item.StartDate, DayOfWeek.Tuesday, weekNumber))
+                    {
+                        lstDateRange.Add(item);
+                    }
+                }
+                if (dayName.ToString().ToLower() == "wednesday")
+                {
+                    if (NthDayOfMonth(item.StartDate, DayOfWeek.Wednesday, weekNumber))
+                    {
+                        lstDateRange.Add(item);
+                    }
+                }
+                if (dayName.ToString().ToLower() == "thursday")
+                {
+                    if (NthDayOfMonth(item.StartDate, DayOfWeek.Thursday, weekNumber))
+                    {
+                        lstDateRange.Add(item);
+                    }
+                }
+                if (dayName.ToString().ToLower() == "friday")
+                {
+                    if (NthDayOfMonth(item.StartDate, DayOfWeek.Friday, weekNumber))
+                    {
+                        lstDateRange.Add(item);
+                    }
+                }
+                if (dayName.ToString().ToLower() == "saturday")
+                {
+                    if (NthDayOfMonth(item.StartDate, DayOfWeek.Saturday, weekNumber))
+                    {
+                        lstDateRange.Add(item);
+                    }
+                }
+                if (dayName.ToString().ToLower() == "sunday")
+                {
+                    if (NthDayOfMonth(item.StartDate, DayOfWeek.Sunday, weekNumber))
+                    {
+                        lstDateRange.Add(item);
+                    }
+                }
+            }
+           
+        }
+
+        #endregion
+    }
+}
